5c10efb075d378d2d84eee8ea85395a555cbc094
Bill Walker Fosuhene <billwalker.bwf@gmail.com>
2026-09-16 21:22:51 +0000

    Add modal window

--- main.js
+++ main.js
@@ -3,11 +3,33 @@ container.classList.add("main-container");
 
 document.body.appendChild(container);
 
+// create grid boxes
 for (let i = 0; i <= 8040; i++) {
   const box = document.createElement("div");
   box.classList.add("per-box");
   container.appendChild(box);
+
+  // change grid color
   box.addEventListener("mouseover", () => {
     box.style.backgroundColor = "white";
   });
 }
+
+// Create modal window
+const showModal = document.querySelector(".show_modal");
+const modal = document.querySelector(".modal");
+const userInput = document.querySelector(".user_value");
+
+// Show modal window
+showModal.addEventListener("click", () => {
+  userInput.classList.remove("hidden");
+  modal.classList.remove("hidden");
+});
+
+// Close modal window
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    userInput.classList.add("hidden");
+    modal.classList.add("hidden");
+  }
+});
